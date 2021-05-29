e72514a1b9254bb9d8b27bc30f25771e745378ca
jcca-1704 <jessicamary1704@gmail.com>
2021-05-29 15:55:05 +0530

    Adding scraper

---
+++ df.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 28 13:23:17 2021
+
+@author: Mary Jessica
+"""
+
+import glassdoor_scrapper as gs
+import pandas as pd
+path = "C:/Users/Mary Jessica/OneDrive/Desktop/DataScience/chromedriver"
+df= gs.get_jobs('Data Scientist',15,False, path,15)
+
+
